695c5fe04983c8f93d03f99d8c3253253c715a1b
Simao Belchior <simaobelchior@gmail.com>
2020-03-17 15:30:35 +0000

    adds missing method

--- app/resources/api/v1/store_resource.rb
+++ app/resources/api/v1/store_resource.rb
@@ -3,6 +3,10 @@ module Api
     class StoreResource < ApplicationResource
       immutable
       attributes :name, :group, :address, :coordinates, :capacity, :details, :store_type
+
+      def coordinates
+        [@model.latitude, @model.longitude]
+      end
     end
   end
 end
