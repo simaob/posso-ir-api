d3138a8de194e03ff1ec539e4792e02f7fe2273a
Tiago Santos <santos.tiago@gmail.com>
2020-03-18 19:24:55 +0100

    Modified query to fetch the supermarkets. Endpoint now works

--- app/models/store.rb
+++ app/models/store.rb
@@ -20,7 +20,7 @@
 #  lonlat     :geometry         point, 0
 #
 class Store < ApplicationRecord
-  RADIUS = 5
+  RADIUS = 5000
   PROJECTION = 4326
 
   # geocoded_by :address
@@ -32,26 +32,23 @@ class Store < ApplicationRecord
 
   # after_validation :reverse_geocode
   # after_validation :geocode
-  before_save :set_lonlat, if: -> { latitude_changed? || longitude_changed? }
+  after_save :set_lonlat
 
   def address
     [street, city].compact.join(',')
   end
 
   def self.retrieve_stores(lat, lon)
-    query=
-        <<~SQL
- SELECT *
-  FROM stores
-  WHERE ST_CONTAINS(
-    ST_BUFFER(
-      ST_SetSRID(
-          ST_MakePoint(#{lon}, #{lat}), 4326)::geography,
-      #{RADIUS})::geometry,
+    query = <<~SQL
+ST_CONTAINS(
+  ST_BUFFER(
+    ST_SetSRID(
+      ST_MakePoint(#{lon}, #{lat}), 4326)::geography,
+    #{RADIUS})::geometry,
     lonlat)
-)))
-    SQL
-    ActiveRecord::Base.connection.execute(query)
+SQL
+
+    Store.where(query)
   end
 
   private
@@ -59,11 +56,13 @@ class Store < ApplicationRecord
   # x: longitude
   # y: latitude
   def set_lonlat
-    self.lonlat = if latitude && longitude
-                    Store.select("ST_MakePoint(#{longitude}, #{latitude}) AS point")
-                      .limit(1).first&.point
-                  else
-                    nil
-                  end
+    return unless persisted? && latitude.present? && longitude.present?
+
+    sql = <<~SQL
+UPDATE stores
+SET lonlat = ST_SetSRID(ST_MakePoint(longitude, latitude), 4326)
+WHERE id = #{id}
+    SQL
+    ActiveRecord::Base.connection.execute sql
   end
 end
