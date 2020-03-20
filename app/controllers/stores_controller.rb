a5a45c51e7a5a0830fe9b8886f032aea6dc98dbb
Simao Belchior <simaobelchior@gmail.com>
2020-03-20 01:50:51 +0000

    Manage authentication

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,4 +1,5 @@
 class StoresController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_store, only: [:show, :edit, :update, :destroy]
 
   # GET /stores
