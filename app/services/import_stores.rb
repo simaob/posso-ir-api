e0c82e762eeb600b7e69b4d1c2fbe69ef4c7ad44
Simao Belchior <simaobelchior@gmail.com>
2020-03-22 10:48:12 +0000

    create multiple tasks for store importing. updates intermarche

--- app/services/import_stores.rb
+++ app/services/import_stores.rb
@@ -1,5 +1,5 @@
 class ImportStores
-  def call
+  def all
     cleanup
     import_auchan
     import_continente
@@ -9,7 +9,7 @@ class ImportStores
     import_mercadona
     import_minipreco
     import_pingodoce
-    puts "#{Store.count} stores added"
+    puts "#{Store.count} total stores"
   end
 
   def cleanup
@@ -17,6 +17,7 @@ class ImportStores
   end
 
   def import_auchan
+    puts "Starting Auchan, we have #{Store.count} total stores"
     src = File.open(Rails.root.join('db', 'files', 'auchan.csv'), 'r')
     file = File.read(src).force_encoding('UTF-8')
     CSV.parse(file, headers:true, skip_blanks: true). each do |csv|
@@ -30,9 +31,11 @@ class ImportStores
         store_type: 1
       )
     end
+    puts "#{Store.count} total stores"
   end
 
   def import_continente
+    puts "Starting Continente, we have #{Store.count} total stores"
     src = File.open(Rails.root.join('db', 'files', 'continente.json'), 'r')
     file = File.read(src).force_encoding('UTF-8')
     JSON.parse(file)["response"]["locations"].each do |row|
@@ -49,46 +52,49 @@ class ImportStores
         store_type: 1
       )
     end
+    puts "#{Store.count} total stores"
   end
 
   def import_corte_ingles
+    puts "Starting Corte Inglés, we have #{Store.count} total stores"
     src = File.open(Rails.root.join('db', 'files', 'elcorte.json'), 'r')
     file = File.read(src).force_encoding('UTF-8')
     JSON.parse(file).each do |row|
-      coordinates = Geocoder.search(row['location'])&.first&.coordinates
-      sleep 0.25 # don't hammer the API
-
       Store.create(
         name: row['name'],
         group: 'El Corte Inglés',
         country: 'PT',
         street: row['location'],
-        latitude: coordinates&.first,
-        longitude: coordinates&.second,
+        latitude: nil,
+        longitude: nil,
         details: row['time'],
         store_type: 1
       )
     end
+    puts "#{Store.count} total stores"
   end
 
   def import_intermarche
+    puts "Starting Intermarché, we have #{Store.count} total stores"
     src = File.open(Rails.root.join('db', 'files', 'intermarche.json'), 'r')
     file = File.read(src).force_encoding('UTF-8')
-    JSON.parse(file).each do |row|
+    JSON.parse(file)['locations'].each do |row|
       Store.create(
-        name: "Intermarché #{row['name']}",
+        name: "Intermarché #{row['data-ga-label']}",
         group: 'Intermarché',
         country: 'PT',
         city: row['name'],
-        street: row['location'],
-        latitude: row['lat'],
-        longitude: row['long'],
+        street: row['data-search'],
+        latitude: row['data-latitude'],
+        longitude: row['data-longitude'],
         store_type: 1
       )
     end
+    puts "#{Store.count} total stores"
   end
 
   def import_lidl
+    puts "Starting Lidl, we have #{Store.count} total stores"
     src = File.open(Rails.root.join('db', 'files', 'lidl.csv'), 'r')
     file = File.read(src).force_encoding('UTF-8')
     CSV.parse(file, headers:true, skip_blanks: true). each do |csv|
@@ -103,9 +109,11 @@ class ImportStores
         store_type: 1
       )
     end
+    puts "#{Store.count} total stores"
   end
 
   def import_mercadona
+    puts "Starting Mercadona, we have #{Store.count} total stores"
     src = File.open(Rails.root.join('db', 'files', 'mercadona.json'), 'r')
     file = File.read(src).force_encoding('UTF-8')
     JSON.parse(file)['tiendasFull'].each do |row|
@@ -122,30 +130,31 @@ class ImportStores
         store_type: 1
       )
     end
+    puts "#{Store.count} total stores"
   end
 
   def import_minipreco
+    puts "Starting Minipreço, we have #{Store.count} total stores"
     src = File.open(Rails.root.join('db', 'files', 'minipreco.json'), 'r')
     file = File.read(src).force_encoding('UTF-8')
 
     JSON.parse(file)['locations'].each do |row|
-      coordinates = Geocoder.search(row['direccionPostal'])&.first&.coordinates
-      sleep 0.25 # don't hammer the API
-
       Store.create(
         name: "Minipreço #{row['nombreVia'].titleize}",
         group: 'Minipreço',
         country: 'PT',
         city: row['localidad'].titleize,
         street: row['direccionPostal'].titleize,
-        latitude: coordinates&.first,
-        longitude: coordinates&.second,
+        latitude: nil,
+        longitude: nil,
         store_type: 1
       )
     end
+    puts "#{Store.count} total stores"
   end
 
   def import_pingodoce
+    puts "Starting Pingo Doce, we have #{Store.count} total stores"
     src = File.open(Rails.root.join('db', 'files', 'pingoDoce.json'), 'r')
     file = File.read(src).force_encoding('UTF-8')
 
@@ -162,5 +171,6 @@ class ImportStores
         store_type: 1
       )
     end
+    puts "#{Store.count} total stores"
   end
 end
--- lib/tasks/stores.rake
+++ lib/tasks/stores.rake
@@ -1,6 +1,42 @@
 namespace :stores do
   desc 'Import stores into database'
   task import: :environment do
-    ImportStores.new.call
+    ImportStores.new.all
+  end
+
+  task cleanup: :environment do
+    ImportStores.new.cleanup
+  end
+
+  task auchan: :environment do
+    ImportStores.new.import_auchan
+  end
+
+  task continente: :environment do
+    ImportStores.new.import_continente
+  end
+
+  task corte_ingles: :environment do
+    ImportStores.new.import_corte_ingles
+  end
+
+  task intermarche: :environment do
+    ImportStores.new.import_intermarche
+  end
+
+  task lidl: :environment do
+    ImportStores.new.import_lidl
+  end
+
+  task mercadona: :environment do
+    ImportStores.new.import_mercadona
+  end
+
+  task minipreco: :environment do
+    ImportStores.new.import_minipreco
+  end
+
+  task pingo_doce: :environment do
+    ImportStores.new.import_pingo_doce
   end
 end
