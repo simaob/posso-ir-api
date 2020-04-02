7a6883d86c19156264c4932f4efcc9f4dbb7cd9e
Simao Belchior <simaobelchior@gmail.com>
2020-04-02 02:09:38 +0100

    more rules and authorization

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,20 +8,28 @@ class Ability
     return if user.user?
 
     can [:show, :edit, :update], User, id: user.id
+    can :read, :map
 
     if user.admin?
       can :manage, :all
 
     elsif user.general_manager?
       can [:new, :create, :edit, :update], Store
+      can [:new, :create], User
+
+      can [:edit, :update], User do |usr|
+        usr == user || !usr.admin?
+      end
+
+      can :manage_state, Store
       can :read, :all
 
     elsif user.contributor?
-
+      can [:new, :create, :edit, :update], Store
+      can :read, Store
 
     elsif user.store_manager?
       can :index, :manage_stores
-      can :read, :map
       can [:read, :edit, :update], Store do |store|
         store.manager_ids.include?(user.id)
       end
