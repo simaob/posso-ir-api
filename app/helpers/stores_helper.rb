6c267af668488765faaa9dea1646de68a354b5d5
Simao Belchior <simaobelchior@gmail.com>
2020-04-03 22:43:30 +0100

    Small adjustment to stores index

--- app/helpers/stores_helper.rb
+++ app/helpers/stores_helper.rb
@@ -1,13 +1,16 @@
 module StoresHelper
-  def badge_for store
-    klass = case store.state
-            when 'live'
-              'badge-success'
-            when 'waiting_approval'
-              'badge-info'
-            when 'marked_for_deletion'
-              'badge-warning'
-            end
-    content_tag(:span, enum_l(store, :state), class: "badge #{klass}")
+  def badge_for(store)
+    content_tag(:span, enum_l(store, :state), class: "badge #{state_klass(store)}")
+  end
+
+  def state_klass(store)
+    case store.state
+    when 'live'
+      'badge-success'
+    when 'waiting_approval'
+      'badge-info'
+    when 'marked_for_deletion'
+      'badge-warning'
+    end
   end
 end
