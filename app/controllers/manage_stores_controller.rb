d45f0aa8fe6df6feb8057dc15d6ebcf41660de61
Simao Belchior <simaobelchior@gmail.com>
2020-04-03 13:38:52 +0100

    paginate manage stores page

--- app/controllers/manage_stores_controller.rb
+++ app/controllers/manage_stores_controller.rb
@@ -2,5 +2,6 @@ class ManageStoresController < ApplicationController
   def index
     @stores = current_user.stores.order(:name)
       .includes(:status_store_owners)
+    @stores = @stores.search(params[:search]).page(params[:page])
   end
 end
