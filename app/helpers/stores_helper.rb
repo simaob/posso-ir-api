97e342c8c8517f9fa1e5d53051919ff12abeb599
Simao Belchior <simaobelchior@gmail.com>
2020-04-03 22:32:40 +0100

    Edit show page active badge

--- app/helpers/stores_helper.rb
+++ app/helpers/stores_helper.rb
@@ -1,2 +1,13 @@
 module StoresHelper
+  def badge_for store
+    klass = case store.state
+            when 'live'
+              'badge-success'
+            when 'waiting_approval'
+              'badge-info'
+            when 'marked_for_deletion'
+              'badge-warning'
+            end
+    content_tag(:span, enum_l(store, :state), class: "badge #{klass}")
+  end
 end
