4b76c5b92333e4b3bf86ffe0f619f5c4918b4b97
Simao Belchior <simaobelchior@gmail.com>
2020-04-04 18:15:05 +0100

    Adds filter by type to statuses

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,5 +1,6 @@
 class StatusesController < ApplicationController
   def index
     @statuses = Status.order(updated_at: :desc).page(params[:page])
+    @statuses = @statuses.where(type: params[:type]) if params[:type].present?
   end
 end
