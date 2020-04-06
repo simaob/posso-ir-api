05a26d163198972ce321869d9bcd8b58154867b6
Simao Belchior <simaobelchior@gmail.com>
2020-04-06 02:30:49 +0100

    Adds travis, renames app

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Covid19ShoppingAssistant
+module PossoIrApi
   class Application < Rails::Application
     config.middleware.insert_before 0, Rack::Cors, debug: !Rails.env.production?,
       logger: (-> { Rails.logger }) do
