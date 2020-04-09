47207e5f72da84267ec4326df9095fe42d7b0a85
Simao Belchior <simaobelchior@gmail.com>
2020-04-10 00:42:12 +0100

    Server time displayed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,8 +15,7 @@ import 'bootstrap'
 import $ from 'jquery';
 import 'select2';
 
-
-import '../stylesheets/application'
+import '../stylesheets/application';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
