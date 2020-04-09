26963c19f8ac8a2902ef19302bad95fae11c2f9c
Simao Belchior <simaobelchior@gmail.com>
2020-04-09 13:47:17 +0100

    Adds two charts to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
-  def index; end
+  def index
+    @users_per_day = ::Api::Charts::Home.new.users_per_day
+    @statuses_per_day = ::Api::Charts::Home.new.statuses_per_day
+  end
 end
