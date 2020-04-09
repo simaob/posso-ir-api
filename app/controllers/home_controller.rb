5c7de3e0404745fc343226bfffd82a7d2918d783
Simao Belchior <simaobelchior@gmail.com>
2020-04-09 21:03:37 +0100

    handle rubocop

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,9 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    if current_user&.admin? || current_user&.general_manager?
-      @users_per_day = ::Api::Charts::Home.new.users_per_day
-      @statuses_per_day = ::Api::Charts::Home.new.statuses_per_day
-    end
+    return unless current_user&.admin? || current_user&.general_manager?
+
+    @users_per_day = ::Api::Charts::Home.new.users_per_day
+    @statuses_per_day = ::Api::Charts::Home.new.statuses_per_day
   end
 end
