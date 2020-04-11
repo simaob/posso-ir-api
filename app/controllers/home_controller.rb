acc4437ad626e0f30cf43a4355a04fa7d0f108bc
Simao Belchior <simaobelchior@gmail.com>
2020-04-11 10:29:09 +0100

    breakdown per day

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,15 +6,27 @@ class HomeController < ApplicationController
 
     @users_per_day = ::Api::Charts::Home.new.users_per_day
     @statuses_per_day = ::Api::Charts::Home.new.statuses_per_day
+    @todays_stores_by_reports = Store.joins(:status_crowdsource_users)
+      .select('stores.*, count(*) AS count')
+      .where(status_crowdsource_users: {created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day})
+      .group('stores.id')
+      .order(count: :desc)
+      .limit(20)
+    @todays_users_by_reports = User.joins(:status_crowdsource_users)
+      .select('users.*, count(*) AS count')
+      .where(status_crowdsource_users: {created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day})
+      .group('users.id')
+      .order(count: :desc)
+      .limit(20)
     @stores_by_reports = Store.joins(:status_crowdsource_users)
       .select('stores.*, count(*) AS count')
       .group('stores.id')
       .order(count: :desc)
-      .limit(50)
+      .limit(40)
     @users_by_reports = User.joins(:status_crowdsource_users)
       .select('users.*, count(*) AS count')
       .group('users.id')
       .order(count: :desc)
-      .limit(50)
+      .limit(40)
   end
 end
