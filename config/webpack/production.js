2e228939e0180c44d125bd9a02f1fa888d4ff32c
Rodrigo Solís <solis.ro@gmail.com>
2020-04-15 23:10:02 +0200

    Revert "don't minimize js"
    This reverts commit 3e98b2033c1eeee425166e3371c71b94a4cdb55c.

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,8 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-const config = environment.toWebpackConfig()
-
-config.optimization.minimize = false
-
-module.exports = config
+module.exports = environment.toWebpackConfig()
