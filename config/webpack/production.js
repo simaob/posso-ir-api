3e98b2033c1eeee425166e3371c71b94a4cdb55c
Simao Belchior <simaobelchior@gmail.com>
2020-04-15 21:49:21 +0100

    don't minimize js

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+const config = environment.toWebpackConfig()
+
+config.optimization.minimize = false
+
+module.exports = config
