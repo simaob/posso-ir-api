979c4d5a55a1e040833b6f8d698f2bc2a572fe43
Simao Belchior <simaobelchior@gmail.com>
2020-04-16 17:59:23 +0100

    misuse of if

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -6,9 +6,10 @@ module Api
         # or otherwise if there's a filter by location present
         if store_owner_code && !context[:current_user]&.store_owner?
           render(json: {error: 'you are not authorized to list all available stores'}, status: 403) and return
-        elsif !params.dig(:filter, :location)
+        elsif !store_owner_code && !params.dig(:filter, :location)
           render(json: {error: 'filter[location]={lat,lng} is mandatory for this request'}, status: 403) and return
         end
+
         super
       end
     end
