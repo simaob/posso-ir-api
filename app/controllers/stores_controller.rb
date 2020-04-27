9aa6965ae6a9872e28f8f93c4c3b3dfc46f13404
Tiago Santos <santos.tiago@gmail.com>
2020-04-27 19:57:19 +0200

    Added the call interval to the stores

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -111,7 +111,7 @@ class StoresController < ApplicationController
   def store_params
     permitted_params = [:name, :group, :street, :city, :zip_code, :country,
                         :district, :store_type, :latitude, :longitude,
-                        :store_type, :open, :capacity, :details,
+                        :store_type, :open, :capacity, :details, :phone_call_interval,
                         phones_attributes: [:id, :phone_number, :name, :active, :_destroy],
                         week_days_attributes: [:id, :opening_hour, :closing_hour, :active]]
     if current_user.admin? || current_user.general_manager?
