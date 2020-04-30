98afebd92390a36a728be7edd291f98fffbea720
Tiago Santos <santos.tiago@gmail.com>
2020-04-30 22:19:30 +0200

    Added the controller for estimations

---
+++ app/controllers/api/v1/status_estimations_controller.rb
@@ -0,0 +1,36 @@
+module Api
+  module V1
+    class StatusEstimationsController < ExternalApiController
+      def create
+        return forbidden unless current_user.admin?
+
+        store = Store.find_by(id: params['id'], active: true).first
+        status = params['status']
+        return wrong_store unless store
+        return invalid_data unless status
+
+        estimation = StatusEstimation.create(status: status,
+                                             store_id: store.id, updated_time: DateTime.now)
+        if estimation.errors.any?
+          render json: {error: estimation.errors.first.message}, status: :unprocessable_entity
+        else
+          render json: {message: "Added status for store: #{store.id}"}, status: 201
+        end
+      end
+
+      private
+
+      def forbidden
+        render json: {error: 'Forbidden'}, status: :forbidden
+      end
+
+      def wrong_store
+        render json: {error: 'Wrong store'}, status: :not_found
+      end
+
+      def invalid_data
+        render json: {error: 'Invalid status'}, status:  :bad_request
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -27,9 +27,9 @@ Rails.application.routes.draw do
       jsonapi_resources :status_crowdsources, only: [:index] do end
       jsonapi_resources :status_generals, only: [:index] do end
       jsonapi_resources :random_status_generals, only: [:index] do end
-      #jsonapi_resources :status_estimations, only: [:create] do end
 
       post 'status-phone', to: 'status_phone#create'
+      post 'status_estimations', to: 'status_estimations#create'
     end
   end
 
