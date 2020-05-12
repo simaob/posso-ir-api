628cab37bea3cae69315e7ee44ca48f25acad4b9
Rodrigo Solís <solis.ro@gmail.com>
2020-05-12 23:35:31 +0200

    prevent re-renders on map and limit fetch to zoom 10

--- app/javascript/components/map/mapbox/map.component.js
+++ app/javascript/components/map/mapbox/map.component.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useState, useCallback } from 'react';
 import { Marker } from 'react-map-gl';
 import { LayerManager, Layer } from 'layer-manager/dist/components';
 import { PluginMapboxGl } from 'layer-manager';
@@ -13,8 +13,32 @@ const initialViewport = {
   minZoom: 6
 };
 
+const MapContent = React.memo(props => {
+  const { map, layers, selectedShop, setDragging, onMarkerDragEnd, dragging } = props;
+  return (
+    <>
+      <LayerManager map={map} plugin={PluginMapboxGl}>
+        {layers.map(l => (
+          <Layer key={l.id} {...l} />
+        ))}
+      </LayerManager>
+      {selectedShop && (
+        <Marker
+          latitude={selectedShop.latitude}
+          longitude={selectedShop.longitude}
+          draggable={['editing', 'creating'].includes(status)}
+          onDragStart={() => setDragging(true)}
+          onDragEnd={onMarkerDragEnd}
+        >
+          <div className={cx('selected-shop-marker', { '-dragging': dragging })} />
+        </Marker>
+      )}
+    </>
+  );
+});
+
 function MapboxMap(props) {
-  const { shops = {}, dispatch, selectedShopId, status, bounds = [], onBoundsChange } = props;
+  const { shops = {}, dispatch, selectedShopId, bounds = [], onBoundsChange } = props;
   const [dragging, setDragging] = useState(false);
   const layers = useMemo(() => {
     const _layers = [];
@@ -37,23 +61,31 @@ function MapboxMap(props) {
     return _layers;
   }, [shops, selectedShopId]);
 
-  const onClick = e => {
-    const shopsMarker = e.features.find(f => f.source === 'shops');
-    if (shopsMarker) {
-      dispatch({ type: 'clickMarker', payload: shopsMarker });
-    } else {
-      dispatch({ type: 'clickMap', payload: { lngLat: e.lngLat } });
-    }
-  };
+  const onClick = useCallback(
+    e => {
+      const shopsMarker = e.features.find(f => f.source === 'shops');
+      if (shopsMarker) {
+        dispatch({ type: 'clickMarker', payload: shopsMarker });
+      } else {
+        dispatch({ type: 'clickMap', payload: { lngLat: e.lngLat } });
+      }
+    },
+    [dispatch]
+  );
 
-  const onMarkerDragEnd = e => {
-    setDragging(false);
-    dispatch({ type: 'dragMarker', payload: e.lngLat });
-  };
+  const onMarkerDragEnd = useCallback(
+    e => {
+      setDragging(false);
+      dispatch({ type: 'dragMarker', payload: e.lngLat });
+    },
+    [setDragging, dispatch]
+  );
+
+  const mapBounds = useMemo(() => ({ bbox: bounds.flat() }), [bounds]);
 
   return (
     <BasicMap
-      bounds={bounds && { bbox: bounds.flat() }}
+      bounds={mapBounds}
       mapboxApiAccessToken={process.env.MAPBOX_TOKEN}
       mapStyle={process.env.NODE_ENV !== 'development' ? process.env.MAPBOX_STYLE : undefined}
       minZoom={2}
@@ -63,24 +95,14 @@ function MapboxMap(props) {
       onBoundsChange={onBoundsChange}
     >
       {map => (
-        <>
-          <LayerManager map={map} plugin={PluginMapboxGl}>
-            {layers.map(l => (
-              <Layer key={l.id} {...l} />
-            ))}
-          </LayerManager>
-          {selectedShopId && (
-            <Marker
-              latitude={shops[selectedShopId].latitude}
-              longitude={shops[selectedShopId].longitude}
-              draggable={['editing', 'creating'].includes(status)}
-              onDragStart={() => setDragging(true)}
-              onDragEnd={onMarkerDragEnd}
-            >
-              <div className={cx('selected-shop-marker', { '-dragging': dragging })} />
-            </Marker>
-          )}
-        </>
+        <MapContent
+          map={map}
+          layers={layers}
+          dragging={dragging}
+          setDragging={setDragging}
+          onMarkerDragEnd={onMarkerDragEnd}
+          selectedShop={selectedShopId && shops[selectedShopId]}
+        />
       )}
     </BasicMap>
   );
