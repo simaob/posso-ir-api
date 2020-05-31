fe62fa3d5cd96864293f7c109ee42e4ccbe326b4
Simao Belchior <simaobelchior@gmail.com>
2020-05-31 12:04:03 +0100

    don't user random on production

--- app/controllers/api/v1/random_stores_controller.rb
+++ app/controllers/api/v1/random_stores_controller.rb
@@ -2,8 +2,12 @@ module Api
   module V1
     class RandomStoresController < StoresController
       def index
-        result = apply_params(records)
-        render json: RandomStoreSerializer.new(result).serialized_json
+        if Rails.env.production?
+          render json: {error: 'Endpoint not found'}, status: :not_found
+        else
+          result = apply_params(records)
+          render json: RandomStoreSerializer.new(result).serialized_json
+        end
       end
     end
   end
