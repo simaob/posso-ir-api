539145c8963aa20398fcdcb981fb7f33a33ee9cc
Simao Belchior <simaobelchior@gmail.com>
2020-05-31 11:46:35 +0100

    fix rubocop warnings and errors

--- app/serializers/random_store_serializer.rb
+++ app/serializers/random_store_serializer.rb
@@ -11,43 +11,43 @@ class RandomStoreSerializer
     rails_blob_path(object.photo, only_path: true) if object.photo.attached?
   end
 
-  attribute :closing_hour do |object|
-    Time.parse(['18:00', '19:00', '22:00', '23:59'].sample)
+  attribute :closing_hour do
+    Time.zone.parse(['18:00', '19:00', '22:00', '23:59'].sample)
   end
-  attribute :opening_hour do |object|
-    Time.parse(['8:00', '9:00', '10:00', '11:00'].sample)
+  attribute :opening_hour do
+    Time.zone.parse(['8:00', '9:00', '10:00', '11:00'].sample)
   end
   attribute :coordinates do |object|
     [object.latitude, object.longitude]
   end
-  attribute :category, if: proc { |object| object.beach? } do |object|
+  attribute :category, if: proc { |object| object.beach? } do
     [:ocean, :river].sample
   end
-  attribute :quality_flag, if: proc { |object| object.beach? } do |object|
+  attribute :quality_flag, if: proc { |object| object.beach? } do
     [true, false].sample
   end
-  attribute :average_users, if: proc { |object| object.beach? } do |object|
+  attribute :average_users, if: proc { |object| object.beach? } do
     rand(10..200)
   end
-  attribute :guarded, if: proc { |object| object.beach? } do |object|
+  attribute :guarded, if: proc { |object| object.beach? } do
     [true, false].sample
   end
-  attribute :first_aid_station, if: proc { |object| object.beach? } do |object|
+  attribute :first_aid_station, if: proc { |object| object.beach? } do
     [true, false].sample
   end
-  attribute :wc, if: proc { |object| object.beach? } do |object|
+  attribute :wc, if: proc { |object| object.beach? } do
     [true, false].sample
   end
-  attribute :showers, if: proc { |object| object.beach? } do |object|
+  attribute :showers, if: proc { |object| object.beach? } do
     [true, false].sample
   end
-  attribute :accessibility, if: proc { |object| object.beach? } do |object|
+  attribute :accessibility, if: proc { |object| object.beach? } do
     [true, false].sample
   end
-  attribute :parking, if: proc { |object| object.beach? } do |object|
+  attribute :parking, if: proc { |object| object.beach? } do
     [true, false].sample
   end
-  attribute :parking_spots, if: proc { |object| object.beach? } do |object|
+  attribute :parking_spots, if: proc { |object| object.beach? } do
     rand(10..80)
   end
   attribute :season_start, if: proc { |object| object.beach? } do |object|
@@ -65,5 +65,4 @@ class RandomStoreSerializer
   attribute :quality_flag, if: proc { |object| object.beach? } do |object|
     object.beach_configuration.quality_flag
   end
-    # cache_options enabled: true, cache_length: 2.hours
 end
