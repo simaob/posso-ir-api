f527bdc756ed633476f42d2d03bd941ca255f5c6
Simao Belchior <simaobelchior@gmail.com>
2020-06-05 00:45:28 +0100

    review comments

--- app/jobs/occupation_update_job.rb
+++ app/jobs/occupation_update_job.rb
@@ -1,16 +1,18 @@
 class OccupationUpdateJob < ApplicationJob
   queue_as :occupation
 
+  CLOSING_TIME = '22:00'.freeze
+  STARTING_TIME = '06:00'.freeze
+
   def perform
     Sidekiq.logger.debug('Starting occupation update')
     ApaFetcher.occupation
-    closing_time = Time.zone.parse('21:00')
+    closing_time = Time.zone.parse(CLOSING_TIME)
     next_update = if Time.current.now >= closing_time
-                    Time.zone.parse('8:00') + 1.day
+                    Time.zone.parse(STARTING_TIME) + 1.day
                   else
                     Time.current + 15.minutes
                   end
-    OccupationUpdateJob.set(wait_until: next_update)
-      .perform_later
+    OccupationUpdateJob.set(wait_until: next_update).perform_later
   end
 end
