253c22affef5e8128846776f6794fa62aca41a33
Simao Belchior <simaobelchior@gmail.com>
2020-06-06 13:44:49 +0100

    comment also on the random store serializer

--- app/serializers/random_store_serializer.rb
+++ app/serializers/random_store_serializer.rb
@@ -6,10 +6,6 @@ class RandomStoreSerializer
   attributes :name, :group, :address, :capacity,
              :details, :store_type, :lonlat, :opening_hour, :closing_hour
 
-  attribute :photo do |object|
-    rails_blob_path(object.photo, only_path: true) if object.photo.attached?
-  end
-
   attribute :closing_hour do
     Time.zone.parse(['18:00', '19:00', '22:00', '23:59'].sample)
   end
@@ -19,14 +15,8 @@ class RandomStoreSerializer
   attribute :coordinates do |object|
     [object.latitude, object.longitude]
   end
-  attribute :category, if: proc { |object| object.beach? } do
-    [:ocean, :river].sample
-  end
   attribute :quality_flag, if: proc { |object| object.beach? } do
     [true, false].sample
-  end
-  attribute :average_users, if: proc { |object| object.beach? } do
-    rand(10..200)
   end
   attribute :guarded, if: proc { |object| object.beach? } do
     [true, false].sample
@@ -40,11 +30,32 @@ class RandomStoreSerializer
   attribute :showers, if: proc { |object| object.beach? } do
     [true, false].sample
   end
+  attribute :parking, if: proc { |object| object.beach? } do
+    [true, false].sample
+  end
+  attribute :water_classification, if: proc { |object| object.beach? } do |object|
+    object.beach_configuration.water_classification.presence || rand(1..4)
+  end
+  attribute :water_quality_updated_at, if: proc { |object| object.beach? } do |object|
+    object.beach_configuration.water_quality_updated_at.presence || (Time.current - 2.hours)
+  end
+  attribute :quality_flag, if: proc { |object| object.beach? } do |object|
+    object.beach_configuration.quality_flag.presence || [true, false].sample
+  end
   attribute :accessibility, if: proc { |object| object.beach? } do
     [true, false].sample
   end
-  attribute :parking, if: proc { |object| object.beach? } do
-    [true, false].sample
+  attribute :water_quality, if: proc { |object| object.beach? } do |object|
+    object.beach_configuration.water_quality.presence || rand(1..4)
+  end
+  attribute :water_quality_url, if: proc { |object| object.beach? } do |object|
+    object.beach_configuration.water_quality_url.presence || 'https://apambiente.pt/'
+  end
+  attribute :category, if: proc { |object| object.beach? } do
+    [:ocean, :river].sample
+  end
+  attribute :average_users, if: proc { |object| object.beach? } do
+    rand(10..200)
   end
   attribute :parking_spots, if: proc { |object| object.beach? } do
     rand(10..80)
@@ -55,20 +66,8 @@ class RandomStoreSerializer
   attribute :season_end, if: proc { |object| object.beach? } do
     Date.parse('30/09/2020')
   end
-  attribute :water_classification, if: proc { |object| object.beach? } do |object|
-    object.beach_configuration.water_classification.presence || rand(1..4)
-  end
-  attribute :water_quality, if: proc { |object| object.beach? } do |object|
-    object.beach_configuration.water_quality.presence || rand(1..4)
-  end
-  attribute :water_quality_updated_at, if: proc { |object| object.beach? } do |object|
-    object.beach_configuration.water_quality_updated_at.presence || (Time.current - 2.hours)
-  end
-  attribute :water_quality_url, if: proc { |object| object.beach? } do |object|
-    object.beach_configuration.water_quality_url.presence || 'https://apambiente.pt/'
-  end
-  attribute :quality_flag, if: proc { |object| object.beach? } do |object|
-    object.beach_configuration.quality_flag.presence || [true, false].sample
+  attribute :photo do |object|
+    rails_blob_path(object.photo, only_path: true) if object.photo.attached?
   end
 
   def address
