1dc031720dd0f9bdb18ef5ff5ae7a998ae073961
Simao Belchior <simaobelchior@gmail.com>
2020-06-09 13:29:03 +0100

    one more reversion

--- app/controllers/external_api_controller.rb
+++ app/controllers/external_api_controller.rb
@@ -14,7 +14,7 @@ class ExternalApiController < ApplicationController
     if DateTime.parse(payload['expiration_date']) <= DateTime.current
       render json: {error: 'Auth token has expired. Please login again'}, status: :unauthorized
     else
-      @current_user = ApiKey.find_by(access_token: api_key).user
+      @current_user = ApiKey.find_by(access_token: token).user
     end
   rescue StandardError
     render json: {error: 'not authorized'}, status: :unauthorized
