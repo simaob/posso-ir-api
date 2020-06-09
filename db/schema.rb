5947312643088f9194a578dd8c5a584661d40310
Simao Belchior <simaobelchior@gmail.com>
2020-06-09 12:18:16 +0100

    adds new field to stores and task to import sapo codes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_085242) do
+ActiveRecord::Schema.define(version: 2020_06_09_110052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -226,6 +226,7 @@ ActiveRecord::Schema.define(version: 2020_06_09_085242) do
     t.string "source"
     t.boolean "make_phone_calls", default: false
     t.integer "phone_call_interval", default: 60
+    t.string "county"
     t.index ["created_by_id"], name: "index_stores_on_created_by_id"
     t.index ["lonlat"], name: "index_stores_on_lonlat", using: :gist
     t.index ["make_phone_calls"], name: "index_stores_on_make_phone_calls"
