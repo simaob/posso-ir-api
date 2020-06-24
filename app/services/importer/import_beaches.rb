19884dc70e206016fea10eefd257e75789ddd647
Simao Belchior <simaobelchior@gmail.com>
2020-06-24 14:22:05 +0100

    remove empty space. adjust styles for forgot password

--- app/services/importer/import_beaches.rb
+++ app/services/importer/import_beaches.rb
@@ -32,7 +32,6 @@ module Importer
         store.name = beach['praia']
         beach_conf = store.beach_configuration
 
-
         beach_conf.code = beach['code']&.to_s
         beach_conf.water_code = beach['codigo_agua_balnear']
         beach_conf.guarded = beach['vigilancia']
