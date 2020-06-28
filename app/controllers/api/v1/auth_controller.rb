41ccdfae2d766b75f8f34f65871dd8e5cc070d4e
Tiago Santos <santos.tiago@gmail.com>
2020-06-29 00:24:39 +0100

    Lists users' favorites on login in

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -40,7 +40,7 @@ module Api
           # rubocop:disable Rails/SkipsModelValidations
           User.where.not(id: user.id).where(app_uuid: user.app_uuid).update_all(app_uuid: nil)
           # rubocop:enable Rails/SkipsModelValidations
-          render json: {success: 'login successful'}, status: :ok
+          render json: FavoriteSerializer.new(user.favorites).serialized_json, status: :ok
         else
           render json: {error: 'wrong password'}, status: :unauthorized
         end
