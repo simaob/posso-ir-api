15737e80101cc3cae87a849d99bd65cfdb06f361
Simao Belchior <simaobelchior@gmail.com>
2020-07-02 11:00:59 +0100

    enable emails with sendgrid ons taging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -104,4 +104,15 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.default_url_options = {host: 'posso-ir-staging.herokuapp.com'}
+
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'yourdomain.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
