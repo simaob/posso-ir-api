e11f3d5927622012bd828b12bd6dd52adfb31287
Simao Belchior <simaobelchior@gmail.com>
2020-07-20 09:28:18 +0100

    Return always at least one

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -71,7 +71,7 @@ module Api
 
       def random_badges_for(user)
         user.user_badges.destroy_all
-        [:welcome, :observer, :addicted, :noob, :beginner].sample(rand(0..4)).each do |badge|
+        [:welcome, :observer, :addicted, :noob, :beginner].sample(rand(1..5)).each do |badge|
           bdg = Badge.find_or_create_by(slug: badge, name: badge.to_s.titleize)
           user.user_badges << UserBadge.new(badge_id: bdg.id, date: rand(1..2).days.ago)
         end
