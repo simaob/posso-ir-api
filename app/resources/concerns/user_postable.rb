c29285778be3da36cb8af1e4001c9c5ff9b09030
Simao Belchior <simaobelchior@gmail.com>
2020-07-21 14:38:22 +0100

    Users can't report the same location more than once per half an hour

--- app/resources/concerns/user_postable.rb
+++ app/resources/concerns/user_postable.rb
@@ -4,13 +4,16 @@ module UserPostable
   extend ActiveSupport::Concern
 
   USER_INTERVAL = 0.30
+  DUPLICATE_REPORTS_INTERVAL = 30
 
   def validate_user_interval
     current_user = context[:current_user]
     return if current_user.admin?
     return if current_user.last_post.nil?
-    return if current_user.last_post.utc < (Time.current - USER_INTERVAL.minutes)
-
+    return if current_user.last_post.utc < (Time.current - USER_INTERVAL.minutes) &&
+      current_user.status_crowdsource_users.where(store_id: context[:store_id])
+      .where('posted_at > ?', (Time.current - DUPLICATE_REPORTS_INTERVAL.minutes))
+      .none?
     raise TooManyRequestsError
   end
 
