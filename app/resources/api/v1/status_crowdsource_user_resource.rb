9839452b857b44d9f135347a68fe3283461e5e01
Simao Belchior <simaobelchior@gmail.com>
2020-07-26 22:24:52 +0100

    change to a guard clause

--- app/resources/api/v1/status_crowdsource_user_resource.rb
+++ app/resources/api/v1/status_crowdsource_user_resource.rb
@@ -27,11 +27,11 @@ module Api
 
       def increase_counters
         store_type = @model.store.store_type
-        if %w(beach supermarket pharmacy restaurant).include?(store_type)
-          report = {id: @model.store.id}
-          report[:type] = "#{store_type}_reports"
-          @model.user.increase_badges_counter(report)
-        end
+        return unless %w(beach supermarket pharmacy restaurant).include?(store_type)
+
+        report = {id: @model.store.id}
+        report[:type] = "#{store_type}_reports"
+        @model.user.increase_badges_counter(report)
       end
     end
   end
